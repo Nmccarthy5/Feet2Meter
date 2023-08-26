1854caaa4de4c43f80e3f52c97c1abd7a49bd4c5
Nmccarthy5 <132273942+Nmccarthy5@users.noreply.github.com>
2023-08-26 15:51:12 -0400

    Add files via upload

---
+++ Feet2Meters.java
@@ -0,0 +1,27 @@
+// Coversion between Feet and Meters Table
+public class Feet2Meters {
+	/**Main Method*/
+	public static void main(String[] args) {
+		// Display table
+		System.out.println(
+			"\nFeet     Meters     |     Meters     Feet\n" +
+			"----------------------------------------------");
+		for (double feet = 1.0, meters = 20.0; feet <= 10.0; feet++, meters += 5) {
+			System.out.printf("%4.1f     ", feet);
+			System.out.printf("%6.3f", footToMeter(feet)); //**Feet first table */
+			System.out.print("     |     ");
+			System.out.printf("%-11.1f", meters);
+			System.out.printf("%7.3f\n", meterToFoot(meters)); //**Meter first table */
+		}
+	}
+	
+	/** Convert from feet to meters */
+	public static double footToMeter(double foot) {
+		return 0.305 * foot;
+	}
+
+	/** Convert from meters to feet */
+	public static double meterToFoot(double meter) {
+		return 3.279 * meter;
+	}
+} 
